c7a82beab987dac5fc2e29e491e8cfe61871c90d
Chaitanya Joglekar <chaitu25@users.noreply.github.com>
2025-10-31 14:23:40 +0000

    Dockerizing the application (#59)
    * Dockerizing the application
    
    * Update docker-compose.yml
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update docker-compose.yml
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update Frontend/notes-frontend/Dockerfile
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update Frontend/notes-frontend/Dockerfile
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update Backend/Dockerfile
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Using named volume
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Frontend/notes-frontend/src/axios/index.js
+++ Frontend/notes-frontend/src/axios/index.js
@@ -2,5 +2,5 @@ import axios from "axios"
 
 export default axios.create({
     // baseURL:"http://localhost:7600"
-    baseURL:"https://gray-sturgeon-veil.cyclic.app"
+    baseURL:"http://localhost:80/api"
 })
